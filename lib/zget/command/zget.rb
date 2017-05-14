0e97f6ef19b542f4b5ea364c7a2e1bbe233d0625
Patricio Mac Adden <patriciomacadden@gmail.com>
2017-05-14 20:09:07 -0300

    Add progress to both zget and zput

--- lib/zget/command/zget.rb
+++ lib/zget/command/zget.rb
@@ -2,7 +2,15 @@ module Zget
   class Zget < Command
     attr_reader :file_or_alias, :output
 
+    CONTENT_LENGTH_PROC = ->(content_length) { @content_length = content_length }
+    PROGRESS_PROC = ->(size) do
+      percentage = size * 100 / @content_length
+      print "\rDownloading: #{percentage}%"
+      puts "\n" if percentage == 100
+    end
+
     def initialize(file_or_alias: nil, output: nil)
+      @content_length = 0
       @file_or_alias = file_or_alias
       @output = output
     end
@@ -25,7 +33,7 @@ module Zget
 
         unless service.nil?
           DNSSD.resolve! service do |r|
-            open url(r) do |f|
+            open url(r), content_length_proc: CONTENT_LENGTH_PROC, progress_proc: PROGRESS_PROC do |f|
               filename = output || f.meta["content-disposition"].split(";").last.split("=").last
               File.write filename, f.read
             end
